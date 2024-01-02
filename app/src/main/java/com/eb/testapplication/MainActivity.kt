35710365bbb338411bb238fb9d2f0c9ef19c4c02
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2024-01-02 15:41:33 +0300

    kotlin composable

--- app/src/main/java/com/eb/testapplication/MainActivity.kt
+++ app/src/main/java/com/eb/testapplication/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Enes")
                 }
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Boncuk $name!",
         modifier = modifier
     )
 }
